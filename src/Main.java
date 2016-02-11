a1fe6809cdcac4043ba5fc684a29d5119a9ab553
Gen Student1 <student1@student.touro.edu>
2016-02-11 18:45:11 -0500

    Modified Snake code

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,8 @@
-import javax.swing.JFrame;
-
 public class Main {
 
 	public static void main(String[] args) {
 
-		//Creating the window with all its awesome snaky features
-		Window f1= new Window();
-		
-		//Setting up the window settings
-		f1.setTitle("Snake");
-		f1.setSize(300,300);
-		f1.setVisible(true);
-		f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);             
-
+		// Creating the window with all its awesome snaky features
+		new Window().setVisible(true);
 	}
 }
